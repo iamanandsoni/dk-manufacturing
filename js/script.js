151846ba9e8c2db81d22d91f3b24c1af9d03eafe
iamanandsoni <anand@1238>
2021-01-21 20:44:59 +0530

    website update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('#HomePageModal').modal('show');
+});
+
+
+// Product Detail Gallery
+
+function myFunction(imgs) {
+	var expandImg = document.getElementById("expandedImg");
+	expandImg.src = imgs.src;
+	expandImg.parentElement.style.display = "block";
+}

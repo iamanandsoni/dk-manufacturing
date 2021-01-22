cddaf2442bca7fd3f00c876e0cf106a0de8fd5fa
iamanandsoni <anand@1238>
2021-01-22 15:30:43 +0530

    Nav Active Class

--- js/script.js
+++ js/script.js
@@ -4,9 +4,16 @@ $(function() {
 
 
 // Product Detail Gallery
-
 function myFunction(imgs) {
 	var expandImg = document.getElementById("expandedImg");
 	expandImg.src = imgs.src;
 	expandImg.parentElement.style.display = "block";
 }
+
+// Active Class via jQuery
+// $(document).ready(function() {
+//     $(".nav-item").click(function() {
+//         $(".nav-item").removeClass("active");
+//         $(this).addClass("active");
+//     });
+// });

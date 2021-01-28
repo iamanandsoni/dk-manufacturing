28e057a8f506ee0fb100af3571e04d492a94a776
iamanandsoni <anand@1238>
2021-01-28 16:57:50 +0530

    modal overlapping fix

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-$('.show').zoomImage();
-$('.show-small-img:first-of-type').css({'border': 'solid 1px #951b25', 'padding': '2px'})
-$('.show-small-img:first-of-type').attr('alt', 'now').siblings().removeAttr('alt')
-$('.show-small-img').click(function () {
-  $('#show-img').attr('src', $(this).attr('src'))
+$('.open').zoomImage();
+$('.open-small-img:first-of-type').css({'border': 'solid 1px #951b25', 'padding': '2px'})
+$('.open-small-img:first-of-type').attr('alt', 'now').siblings().removeAttr('alt')
+$('.open-small-img').click(function () {
+  $('#open-img').attr('src', $(this).attr('src'))
   $('#big-img').attr('src', $(this).attr('src'))
   $(this).attr('alt', 'now').siblings().removeAttr('alt')
   $(this).css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
@@ -18,14 +18,14 @@ $('.show-small-img').click(function () {
 })
 // 点击 '>' 下一张
 $('#next-img').click(function (){
-  $('#show-img').attr('src', $(".show-small-img[alt='now']").next().attr('src'))
-  $('#big-img').attr('src', $(".show-small-img[alt='now']").next().attr('src'))
-  $(".show-small-img[alt='now']").next().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
-  $(".show-small-img[alt='now']").next().attr('alt', 'now').siblings().removeAttr('alt')
+  $('#open-img').attr('src', $(".open-small-img[alt='now']").next().attr('src'))
+  $('#big-img').attr('src', $(".open-small-img[alt='now']").next().attr('src'))
+  $(".open-small-img[alt='now']").next().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
+  $(".open-small-img[alt='now']").next().attr('alt', 'now').siblings().removeAttr('alt')
   if ($('#small-img-roll').children().length > 4) {
-    if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
-      $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
-    } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
+    if ($(".open-small-img[alt='now']").index() >= 3 && $(".open-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(".open-small-img[alt='now']").index() - 2) * 76 + 'px')
+    } else if ($(".open-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
       $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
     } else {
       $('#small-img-roll').css('left', '0')
@@ -34,14 +34,14 @@ $('#next-img').click(function (){
 })
 // 点击 '<' 上一张
 $('#prev-img').click(function (){
-  $('#show-img').attr('src', $(".show-small-img[alt='now']").prev().attr('src'))
-  $('#big-img').attr('src', $(".show-small-img[alt='now']").prev().attr('src'))
-  $(".show-small-img[alt='now']").prev().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
-  $(".show-small-img[alt='now']").prev().attr('alt', 'now').siblings().removeAttr('alt')
+  $('#open-img').attr('src', $(".open-small-img[alt='now']").prev().attr('src'))
+  $('#big-img').attr('src', $(".open-small-img[alt='now']").prev().attr('src'))
+  $(".open-small-img[alt='now']").prev().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
+  $(".open-small-img[alt='now']").prev().attr('alt', 'now').siblings().removeAttr('alt')
   if ($('#small-img-roll').children().length > 4) {
-    if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
-      $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
-    } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
+    if ($(".open-small-img[alt='now']").index() >= 3 && $(".open-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(".open-small-img[alt='now']").index() - 2) * 76 + 'px')
+    } else if ($(".open-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
       $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
     } else {
       $('#small-img-roll').css('left', '0')
